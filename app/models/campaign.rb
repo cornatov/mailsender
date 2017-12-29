b6b94de0a26cda548d87fac2c3c5b874e9dd0cee
Cornato Vella <cornatov@buffalo.edu>
2017-12-29 14:53:34 -0800

    Revert "added subject and body presence validation for campaign and campaign mailer"
    This reverts commit 83e00f423c45089a40f1f4f555960652ea2485bc.

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,15 +1,2 @@
 class Campaign < ApplicationRecord
-	#Validations to make sure every blank is filled
-	validates :to_email, Presence: true
-	validates :subject, presence: true
-	validates :body, presence: true
-
-	# Callbacks
-	After_create :send_campaign
-
-	def send_campaign
-		CampaignMailer.send_campaign(self).deliver!
-	end
 end
-
-
