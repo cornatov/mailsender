48563fea05fd195d73289a205e317ce126fa80a7
Cornato Vella <cornatov@buffalo.edu>
2017-12-29 15:40:14 -0800

    campaign.send_campaign now called on create callback and readded added subject and body presence validation for campaign

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,2 +1,16 @@
 class Campaign < ApplicationRecord
+	validates :to_email, presence: true
+	validates :subject, presence: true
+	validates :body, presence: true
+
+	# Callbacks
+	after_create :send_campaign
+
+	def send_campaign
+		CampaignMailer.send_campaign(self).deliver!
+	end
+
+
 end
+
+
